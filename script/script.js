0226f544a8bbd7d74f017496ddb0659c29cd171d
mahtabkhajir1376 <mahtabkhajir0@gmail.com>
2025-09-23 17:40:54 +0330

    create special offer section

--- script/script.js
+++ script/script.js
@@ -1,23 +1,34 @@
-    var swiper = new Swiper(".storySwiper", {
- 
-      navigation: {
-        nextEl: ".story-next",
-        prevEl: ".story-prev",
-      },
-      slidesPerView: 12.5,
-         spaceBetween: 30,
-          slidesPerGroup: 2,  
-           freeMode: true, 
-    });
+var swiper = new Swiper(".storySwiper", {
+  navigation: {
+    nextEl: ".story-next",
+    prevEl: ".story-prev",
+  },
+  slidesPerView: 12.5,
+  spaceBetween: 30,
+  slidesPerGroup: 2,
+  freeMode: true,
+});
 
-    var swiper =new Swiper(".heroSwiper",{
-      navigation:{
-        nextEl:".hero-next",
-        prevEl:".hero-prev"
-      },
-      slidesPerView:1,
-    
-       pagination: {
-        el: ".swiper-pagination",
-      },
-    })
+var swiper = new Swiper(".heroSwiper", {
+  navigation: {
+    nextEl: ".hero-next",
+    prevEl: ".hero-prev",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+
+  slidesPerView: 1,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+var swiper = new Swiper(".special-offer", {
+  navigation: {
+    nextEl: ".special-offer-next",
+    prevEl: ".special-offer-prev",
+  },
+  slidesPerView: 8,
+  spaceBetween: 2,
+});
